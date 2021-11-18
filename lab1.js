424170bf1490225109358dcd064786d401e2b092
Alexander Culp <alextubba@gmail.com>
2021-11-18 12:44:25 -0500

    first commit

---
+++ lab1.js
@@ -0,0 +1,18 @@
+var secondsInAMinute = 60;
+var minutesInAHours = 60;
+var hoursInADay = 24;
+var daysInAYear = 365;
+
+var secondInAnHour = secondsInAMinute * minutesInAHours;
+document.writeln("The number of seconds in an hour is " + secondInAnHour);
+document.writeln("<br>")
+
+var secondsInADay = secondInAnHour * hoursInADay;
+document.writeln("The number of seconds in a day are " + secondsInADay);
+document.writeln("<br>");
+
+var secondsInAYear = secondsInADay * daysInAYear;
+document.writeln("The number of seconds in a year are " + secondsInAYear);
+document.writeln("<br>");
+document.writeln("<br>");
+document.writeln("Created my Mr sekol's favorite student")
